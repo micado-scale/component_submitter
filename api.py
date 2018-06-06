bbc6b2396795b068d01c43b759692630630d33d7
gregGes <gesmier.gregoire@gmail.com>
2018-06-06 13:29:32 +0100

    update api with the correct update method

--- api.py
+++ api.py
@@ -4,8 +4,6 @@ from abstracts.exceptions import AdaptorCritical
 from werkzeug import secure_filename
 import os
 app = Flask(__name__)
-UPLOAD_FOLDER="/Users/greg/Desktop/work/COLA/submitter/greg_fork/component_submitter/files/tests/"
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 import logging
 import ast
 import utils
@@ -96,22 +94,9 @@ def engine_url():
 
 @app.route('/v1.0/app/launch/file/', methods=['POST'])
 def engine_file():
-    """ API functions to launch a application
-
-        stream.read() file
-
+    """ API functions to launch an application
     """
-    # try:
-    #     f = request.files
-    #     logger.debug(f)
-    #
-    # except Exception as e:
-    #     logger.debug(e)
-    #     return e.message
-    # #f.save(secure_filename(f.filename))
-    # return 'file uploaded successfully'
-    #logger.debug("template: {}".format(template_yaml))
-    f = request.files['file']
+    template = request.files['file']
     try:
          id_app= request.form['id']
     except Exception:
@@ -124,30 +109,14 @@ def engine_file():
     else:
         parsed_params = ast.literal_eval(params)
 
-    f.save("{}/files/templates/{}.yaml".format(app.root_path,id_app))
+    template.save("{}/files/templates/{}.yaml".format(app.root_path,id_app))
     path_to_file = "files/templates/{}.yaml".format(id_app)
 
     id_app = submitter.launch(path_to_file = path_to_file, parsed_params=parsed_params, id_app=id_app)
     response = jsonify(dict(message="app:{}".format(id_app)))
     response.status_code = 200
     return response
-    #return "True"
-# @app.route('/v1.0/app/launch/file/', methods=['POST'])
-# def engine_file_no_id():
-#     f = request.files['file']
-#
-#
-#     logger.debug(f)
-#     return "true"
-# #    parsed_params = request.form['params']
-#     #app_id = request.form['id']
-#     #template_yaml = request.stream.read()
-#     #logger.debug(template_yaml)
-#     #app_id = utils.id_generator()
-#     #utils.dump_order_yaml(yaml.safe_load(template_yaml), "files/templates/{}.yaml".format(app_id))
-#     #logger.debug("template is: {}".format(app_id))
-#     #return "True"
-#
+
 
 
 @app.route('/v1.0/app/undeploy/<id_app>', methods=['DELETE'])
@@ -160,9 +129,9 @@ def undeploy(id_app):
     else:
         return jsonify(dict(message="something undexpected happened", status_code=500))
 
-@app.route('/v1.0/app/update/<id_app>', methods=['PUT'])
-def update(id_app):
-    """ API function to deploy the application with a specific ID """
+@app.route('/v1.0/app/update/url/<id_app>', methods=['PUT'])
+def update_url(id_app):
+    """ API function to update the application with a specific ID from a url"""
     logger.info("id is: {}".format(id_app))
     path_to_file = request.form['input']
     logger.info("path_to_file is: {}".format(path_to_file))
@@ -184,18 +153,36 @@ def update(id_app):
         else:
             return jsonify(dict(message="update failed", status_cod=500))
 
+@app.rout('/v1.0/app/update/file/<id_app>', methods=['PUT'])
+def update_file(id_app):
+    """ API function to update the application with a specific ID from a file"""
+    template = request.files['file']
+    try:
+         params= request.form['params']
+    except Exception:
+         parsed_params = None
+    else:
+        parsed_params = ast.literal_eval(params)
 
+    template.save("{}/files/templates/{}.yaml".format(app.root_path,id_app))
+    path_to_file = "files/templates/{}.yaml".format(id_app)
+    response = submitter.update(id_app, path_to_file=path_to_file, parsed_params=parsed_params)
+    if response is None:
+        return jsonify(dict(message="correctly updated", status_code=200))
+    else:
+        return jsonify(dict(message="update failed", status_cod=500))
 
 @app.route('/v1.0/app/<id_app>', methods=['GET'])
 def info_app(id_app):
+    """ API function to get the information on a given id """
     response = jsonify(dict(message="{}".format(submitter.app_list[id_app])))
     response.status_code = 500
-
 
     return response
 
 @app.route('/v1.0/list_app', methods=['GET'])
 def list_app():
+    """ API function to list all the running aplications"""
     response = []
     for key, value in submitter.app_list.items():
         response.append("id: {}, outputs: {}\n".format(key,value))
