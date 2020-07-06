f3c13aeb9fbec88694524ce9f825e366a622b61f
jaydesl <jay.deslauriers@gmail.com>
2020-07-06 20:29:00 +0100

    Improve toscaparser error reports

--- micado_parser.py
+++ micado_parser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from toscaparser.tosca_template import ToscaTemplate
+from toscaparser.common.exception import ValidationError
 import os
 import toscaparser.utils.urlutils
 import sys
@@ -9,62 +10,72 @@ import micado_validator as Validator
 import inspect
 import traceback
 
-logger=logging.getLogger("submitter."+__name__)
+logger = logging.getLogger("submitter." + __name__)
 
 
 class MiCADOParser(object):
-  """
-  MiCADO class that is going to take care of parsing the topology template, check if the
-  file is correct and Readable by MiCADO submitter. The set_template method is returning
-  the topology template object.
-  """
+    """ Class that is going to take care of parsing the topology template,
 
-  def __init__(self):
+    check if the file is correct and Readable by MiCADO submitter. The
+    set_template method is returning the topology template object.
     """
-    constructor, instantiate the class but doesn't do anything else.
-    """
-    logger.debug("Initialisation of the MiCADO Parser")
 
-  def set_template(self,path, parsed_params=None):
-    """
-    set_template is the method that will parse the tosca template and return the
-    object topology object.
+    def __init__(self):
+        """
+        Constructor, instantiate the class but doesn't do anything else.
+        """
+        logger.debug("Initialisation of the MiCADO Parser")
 
-    :params: path, parsed_params
-    :type: string, dictionary
-    :return: template
-    :raises: Exception
+    def set_template(self, path, parsed_params=None):
+        """ the method that will parse the YAML and return ToscaTemplate
+        object topology object.
 
-    | parsed_params: dictionary containing the input to change
-    | path: local or remote path to the file to parse (it needs to be reachable.)
-    """
-    self.path = path
-    isfile = False
-    if os.path.isfile(self.path):
-      logger.debug("check if the input file is local")
-      isfile = True
-      #return ToscaTemplate(self.path, parsed_params, isfile)
-    else:
-      try:
-        toscaparser.utils.urlutils.UrlUtils.validate_url(self.path)
-        logger.debug("check if the input is a valid url")
+        :params: path, parsed_params
+        :type: string, dictionary
+        :return: template
+        :raises: Exception
+
+        | parsed_params: dictionary containing the input to change
+        | path: local or remote path to the file to parse
+        """
+        self.path = path
         isfile = False
-      except Exception as e:
-        logger.error("the input file doesn't exist or cannot be reached")
-        raise Exception("Cannot find input file {}".format(e))
+        if os.path.isfile(self.path):
+            logger.debug("check if the input file is local")
+            isfile = True
+        else:
+            try:
+                toscaparser.utils.urlutils.UrlUtils.validate_url(self.path)
+                logger.debug("check if the input is a valid url")
+                isfile = False
+            except Exception as e:
+                logger.error(
+                    "the input file doesn't exist or cannot be reached"
+                )
+                raise Exception("Cannot find input file {}".format(e))
+            
+        try:
+            template = ToscaTemplate(self.path, parsed_params, isfile)
+        except ValidationError as e:
+            message = [
+                line
+                for line in e.message.splitlines()
+                if not line.startswith("\t\t")
+            ]
+            message = "\n".join(message)
+            raise Exception(message) from None
+        except AttributeError as e:
+            logger.error(
+                f"error happened: {e}, This might be due to the wrong type in "
+                "the TOSCA template, check if all the type exist or that the "
+                "import section is correct."
+            )
+            raise Exception(
+                "An error occured while parsing, This might be due to the a "
+                "wrong type in the TOSCA template, check if all the types "
+                "exist, or that the import section is correct."
+            ) from None
 
-    try:
-        template = ToscaTemplate(self.path, parsed_params, isfile)
-    except AttributeError as e:
-        logger.error("error happened: {}, This might be due to the wrong type in the TOSCA template, check if all the type exist or that the import section is correct.".format(e))
-        raise Exception("An error occured while parsing, This might be due to the a wrong type in the TOSCA template, check if all the types exist, or that the import section is correct.")
+        Validator.validation(template)
+        return template
 
-
-
-
-    Validator.validation(template)
-
-
-    #    raise Exception("an error happened most likely with the policy, check if import section is right")
-
-    return template
