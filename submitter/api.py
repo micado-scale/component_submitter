0080a1ec873b907f0a2f06cbd53f52e60e63b85d
jaydesl <jay.deslauriers@gmail.com>
2020-07-30 08:56:42 +0100

    Turn off 404 suggestions

--- submitter/api.py
+++ submitter/api.py
@@ -4,5 +4,6 @@ from submitter.apis.apiv1 import apiv1
 from submitter.apis.apiv2 import v2blueprint
 
 app = Flask(__name__)
+app.config["ERROR_404_HELP"] = False
 app.register_blueprint(apiv1)
 app.register_blueprint(v2blueprint, url_prefix="/v2.0/")
