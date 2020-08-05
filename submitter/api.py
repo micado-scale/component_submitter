98fdc3d98d554a8a2fe80d5ff1144561afcce2ea
jaydesl <jay.deslauriers@gmail.com>
2020-08-05 14:52:55 +0100

    Use vanilla Flask

--- submitter/api.py
+++ submitter/api.py
@@ -1,19 +1,10 @@
 from flask import Flask
 from werkzeug.exceptions import HTTPException
 
-app = Flask(__name__)
-
 from submitter.apis.apiv1 import apiv1
 from submitter.apis.apiv2 import v2blueprint
 
-app.config["ERROR_404_HELP"] = False
+app = Flask(__name__)
+
 app.register_blueprint(apiv1)
 app.register_blueprint(v2blueprint, url_prefix="/v2.0/")
-
-
-# The flask-restx errorhandler is broken so
-# we use the flask errorhandler to pick up exceptions
-@app.errorhandler(HTTPException)
-def default_flask_error_handler(error):
-    """Default error handler for HTTPException"""
-    return {"message": str(error)}, getattr(error, "code", 500)
--- submitter/apis/apiv2.py
+++ submitter/apis/apiv2.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, abort
-from flask_restx import Api, Resource
+from flask.views import MethodView
 from webargs import flaskparser, fields, core
 from webargs.flaskparser import use_kwargs
 from werkzeug.exceptions import HTTPException
@@ -8,12 +8,6 @@ from submitter.apis.core import Applications
 from submitter.utils import id_generator
 
 v2blueprint = Blueprint("apiv2", __name__)
-api = Api(
-    app=v2blueprint,
-    title="MiCADO Submitter REST API",
-    version="2.0",
-    description="An API for applications in MiCADO",
-)
 
 json_args = {
     "adt": fields.Dict(),
@@ -29,47 +23,29 @@ form_args = {
 file_args = {"adt": fields.Field()}
 
 
-@api.errorhandler(HTTPException)
-def internal_error_handler(error):
-    """Re-raise all HTTPExceptions for Flask errorhandler """
-    raise error
-
-
 @flaskparser.parser.error_handler
 def webargs_fix(error, req, schema, *, error_status_code, error_headers):
     abort(
-        error_status_code or core.DEFAULT_VALIDATION_STATUS,
-        error.messages,
+        error_status_code or core.DEFAULT_VALIDATION_STATUS, error.messages,
     )
 
 
-@api.route("/application/")
-@api.response(500, "Internal Error: Submitter Engine")
-class ApplicationList(Resource):
-    @api.doc("list_applications")
-    def get(self):
-        """
-        Return a list of deployed applications
-        """
-        return Applications().get()
-
-    @api.doc("create_application")
-    @use_kwargs(json_args, location="json")
-    @use_kwargs(file_args, location="files")
-    @use_kwargs(form_args, location="form")
-    def post(self, adt=None, url=None, params=None, dryrun=False):
-        """
-        Create a new application with a generated ID
-        """
-        app_id = id_generator()
-        return Applications(app_id).create(adt, url, params, dryrun)
+@v2blueprint.errorhandler(Exception)
+def unhandled_error_handler(error):
+    """Default error handler for unhandled exceptions"""
+    return (
+        {"message": "Unhandled exception: " + str(error)},
+        getattr(error, "code", 500),
+    )
 
 
-@api.route("/application/<app_id>/")
-@api.param("app_id", "The application identifier")
-@api.response(404, "Application not found")
-class Application(Resource):
-    @api.doc("get_application")
+@v2blueprint.errorhandler(HTTPException)
+def http_error_handler(error):
+    """Default error handler for HTTP exceptions"""
+    return ({"message": str(error)}, getattr(error, "code", 500))
+
+
+class Application(MethodView):
     def get(self, app_id):
         """
         Fetch the application matching the given ID
@@ -83,6 +59,8 @@ class Application(Resource):
         """
         Create a new application with a given ID
         """
+        if not app_id:
+            app_id = id_generator()
         return Applications(app_id).create(adt, url, params, dryrun)
 
     @use_kwargs(json_args, location="json")
@@ -101,34 +79,36 @@ class Application(Resource):
         return Applications(app_id).delete(force)
 
 
-@api.route("/application/<app_id>/status")
-@api.param("app_id", "The application identifier")
-@api.response(404, "Application not found")
-class ApplicationStatus(Resource):
-    @api.doc("get_application_status")
+class ApplicationStatus(MethodView):
     def get(self, app_id):
         """
         Fetch the deployment status of an application
         """
 
 
-@api.route("/application/<app_id>/nodes")
-@api.param("app_id", "The application identifier")
-@api.response(404, "Application not found")
-class ApplicationNodes(Resource):
-    @api.doc("get_application_nodes")
+class ApplicationNodes(MethodView):
     def get(self, app_id):
         """
         Fetch the deployed nodes of an application
         """
 
 
-@api.route("/application/<app_id>/services")
-@api.param("app_id", "The application identifier")
-@api.response(404, "Application not found")
-class ApplicationServices(Resource):
-    @api.doc("get_application_services")
+class ApplicationServices(MethodView):
     def get(self, app_id):
         """
         Fetch the deployed services of an application
         """
+
+
+app_view = Application.as_view("apps_api")
+v2blueprint.add_url_rule(
+    "/applications/",
+    view_func=app_view,
+    defaults={"app_id": None},
+    methods=["GET", "POST"],
+)
+v2blueprint.add_url_rule(
+    "/applications/<string:app_id>/",
+    view_func=app_view,
+    methods=["GET", "POST", "PUT", "DELETE"],
+)
