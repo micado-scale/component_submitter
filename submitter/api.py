e438910d12ef92a7c0922fd193a9b34f2c795039
jaydesl <jay.deslauriers@gmail.com>
2020-08-07 13:03:51 +0100

    Add Swagger-ui docs

--- submitter/api.py
+++ submitter/api.py
@@ -10,6 +10,16 @@ app.register_blueprint(v1blueprint)
 app.register_blueprint(v2blueprint, url_prefix="/v2.0/")
 
 
+@app.route('/v2.0/docs/openapi.json')
+def host_openapi():
+    return app.send_static_file('openapi.json')
+
+
+@app.route('/v2.0/swagger')
+def host_swagger():
+    return app.send_static_file('index.html')
+
+
 @app.errorhandler(HTTPException)
 def not_found_errors(error):
     return (
