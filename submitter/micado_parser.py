23efdcd11f950b1e88eb6233c4d498d1d33bf6a4
jaydesl <jay.deslauriers@gmail.com>
2021-03-25 13:13:33 +0000

    refactor: move extra tosca handling to own module

--- submitter/micado_parser.py
+++ submitter/micado_parser.py
@@ -14,7 +14,8 @@ from toscaparser.tosca_template import ToscaTemplate
 from toscaparser.common.exception import ValidationError
 
 from submitter import micado_validator as Validator
-from submitter.utils import resolve_get_inputs, dump_order_yaml
+from submitter.utils import dump_order_yaml, resolve_get_inputs
+from submitter.handle_extra_tosca import resolve_occurrences
 
 logger = logging.getLogger("submitter." + __name__)
 
@@ -54,7 +55,7 @@ def set_template(path, parsed_params=None):
     | path: local or remote path to the file to parse
     """
     tpl = TemplateLoader(path)
-    _resolve_occurrences(tpl.dict, parsed_params)
+    resolve_occurrences(tpl.dict, parsed_params)
 
     with NamedTemporaryFile(dir=tpl.parent_dir, suffix=".yaml") as temp_tpl:
         dump_order_yaml(tpl.dict, temp_tpl.name)
@@ -90,78 +91,6 @@ def get_template(path, parsed_params):
             "exist, or that the import section is correct."
         ) from None
     return template
-
-
-def _determine_inputs(tpl_dict, parsed_params):
-    """
-    Store input values from parsed_params or defaults
-    """
-    params = parsed_params if parsed_params else {}
-    inputs = tpl_dict.get("topology_template", {}).get("inputs", {})
-    values = {}
-    for key, value in inputs.items():
-        values[key] = params.get(key) or value["default"]
-    return values
-
-
-def _resolve_occurrences(tpl_dict, parsed_params):
-    """
-    Handle TOSCA v1.3 occurrences feature, for now
-    """
-    nodes = tpl_dict.get("topology_template", {}).get("node_templates")
-    inputs = _determine_inputs(tpl_dict, parsed_params)
-    nodes_with_occurrences = [
-        node for node in nodes if "occurrences" in nodes[node]
-    ]
-    print(nodes_with_occurrences)
-    for node in nodes_with_occurrences:
-        new_nodes = _create_occurrences(node, nodes.pop(node), inputs)
-        nodes.update(new_nodes)
-
-
-def _create_occurrences(name, node, inputs):
-    """
-    Create and return a dictionary of new occurrences
-    """
-    occurrences = node.pop("occurrences")
-    count = node.pop("instance_count", occurrences[0])
-    if isinstance(count, str):
-        count = int(count)
-    elif isinstance(count, dict):
-        try:
-            count = int(inputs[count["get_input"]])
-        except (KeyError, TypeError):
-            raise KeyError("Could not resolve instance_count")
-
-    new_nodes = {}
-    for i in range(count):
-        new_name = f"{name}-{i+1}"
-        new_node = copy.deepcopy(node)
-        resolve_get_inputs(
-            new_node,
-            _set_indexed_input,
-            lambda x: isinstance(x, list),
-            inputs,
-            i,
-        )
-
-        new_nodes[new_name] = new_node
-
-    return new_nodes
-
-
-def _set_indexed_input(result, inputs, index):
-    """
-    Set the value of indexed inputs
-    """
-    if result[1] != "INDEX":
-        raise TypeError("Unrecognised get_input format")
-    try:
-        return inputs[result[0]][index]
-    except IndexError:
-        raise IndexError(
-            f"Input '{result[0]}' does not match node occurrences!"
-        ) from None
 
 
 def _find_other_inputs(template):
