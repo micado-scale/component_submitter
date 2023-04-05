a38ecdd1f2bcc6c2d0036abc20e889e3ad25861f
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-04-05 19:22:31 +0100

    refactor: simplify get endpoint

--- tests/test_occopus_adaptor.py
+++ tests/test_occopus_adaptor.py
@@ -3,7 +3,7 @@ import unittest
 from micadoparser.parser import set_template
 
 from submitter.adaptors.occopus_adaptor import OccopusAdaptor
-
+from submitter.adaptors import occopus_adaptor as occo
 
 class TestOccopusAdaptor(unittest.TestCase):
     """UnitTests for Resource class"""
@@ -18,6 +18,13 @@ class TestOccopusAdaptor(unittest.TestCase):
 
 
     def test_resource_keys(self):
+        self.assertListEqual(["endpoint", "type",'regionname', 'image_id', 'instance_type', 'key_name', 'security_group_ids'], list(self.worker["resource"].keys()))
+
     def test_health_check(self):
         healthcheck = {"ping": False}
         self.assertEqual(self.worker["health_check"], healthcheck)
+        
+    def test_endpoint_from_interface(self):
+        endpoint = "ADD_YOUR_ENDPOINT (e.g https://ec2.eu-west-1.amazonaws.com)"
+        test = {"create": {"endpoint": endpoint}}
+        self.assertEqual(occo.get_endpoint_from_interface(test), endpoint)
