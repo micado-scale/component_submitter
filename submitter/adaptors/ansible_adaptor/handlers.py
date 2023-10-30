0254f9819a8b7b7971991886264e7a9608c47669
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-10-30 09:52:21 +0000

    refactor(ansib): refactor edge handler

--- submitter/adaptors/ansible_adaptor/handlers.py
+++ submitter/adaptors/ansible_adaptor/handlers.py
@@ -3,7 +3,6 @@ import os
 import shutil
 
 import submitter.adaptors.ansible_adaptor.templates as templates
-from submitter.adaptors.ansible_adaptor.playbook import Playbook
 
 jinja_loader = jinja2.FileSystemLoader(searchpath=templates.__path__)
 jinja_env = jinja2.Environment(loader=jinja_loader)
@@ -17,12 +16,14 @@ def handle_edge_playbook(nodes, out_path, config):
     VERSION = "v0.12.0"
 
     edge_info = get_edge_node_info(nodes)
-
-    edge_path = os.path.join(out_path, "micado-edge")
-
     if not edge_info:
         return
     edge_info["micado_host"] = config.get("micado_host", "localhost")
+
+    edge_path = os.path.join(out_path, "micado-edge")
+    prepare_edge_playbook(VERSION, edge_path)
+
+    write_private_key(edge_info["edges"], edge_path)
 
     template = jinja_env.get_template(f"micado-edge/{VERSION}/hosts.yml.j2")
     template = template.render(**edge_info)
@@ -36,25 +37,36 @@ def handle_edge_playbook(nodes, out_path, config):
 
     return (edge_path, "edge.yml")
 
+def write_private_key(edges, out_path):
+    """Write private keys to files"""
+    for edge, props in edges.items():
+        if not props.get("ssh_private_key"):
+            continue
+        
+        with open(os.path.join(out_path, f"{edge}.pem"), 'w') as f:
+            f.write(props["ssh_private_key"])
+
 def prepare_edge_playbook(version, out_path):
-    micado_playbook = Playbook(
-        url=f"https://github.com/micado-scale/ansible-micado/tarball/{version}"
+    """Copy edge playbook to output directory"""
+    shutil.copytree(
+        os.path.join(templates.__path__[0], f"micado-edge/{version}/playbook"),
+        out_path,
+        dirs_exist_ok=True
     )
-    micado_playbook.download()
-    micado_playbook.extract(out_path)
 
 def get_edge_node_info(nodes):
+    """Get edge node information"""
     NODE_TYPES = ["tosca.nodes.MiCADO.Edge"]
     edges = [node for node in nodes if node.type in NODE_TYPES]
-    return {"edges": { 
-        edge.name: {
-            property: edge.get_property_value(property)
-            for property in edge.get_properties()
+    return {
+        "edges": { 
+            edge.name: {
+                property: edge.get_property_value(property)
+                for property in edge.get_properties()
+            }
+            for edge in edges
         }
-        for edge in edges
     }
-    }
-
 
 
 HANDLERS = {
